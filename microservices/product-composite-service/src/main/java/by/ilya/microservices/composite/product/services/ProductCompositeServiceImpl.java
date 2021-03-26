b2d40b4c47b5ade5a97353e8a2692ba2699be8da
Ilya <mmf.kazakevich@mail.ru>
2021-03-26 13:46:59 +0300

    first commit

---
+++ microservices/product-composite-service/src/main/java/by/ilya/microservices/composite/product/services/ProductCompositeServiceImpl.java
@@ -0,0 +1,67 @@
+package by.ilya.microservices.composite.product.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import by.ilya.api.composite.product.*;
+import by.ilya.api.core.product.Product;
+import by.ilya.api.core.recommendation.Recommendation;
+import by.ilya.api.core.review.Review;
+import by.ilya.util.exceptions.NotFoundException;
+import by.ilya.util.http.ServiceUtil;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class ProductCompositeServiceImpl implements ProductCompositeService {
+
+    private final ServiceUtil serviceUtil;
+    private  ProductCompositeIntegration integration;
+
+    @Autowired
+    public ProductCompositeServiceImpl(ServiceUtil serviceUtil, ProductCompositeIntegration integration) {
+        this.serviceUtil = serviceUtil;
+        this.integration = integration;
+    }
+
+    @Override
+    public ProductAggregate getProduct(int productId) {
+
+        Product product = integration.getProduct(productId);
+        if (product == null) throw new NotFoundException("No product found for productId: " + productId);
+
+        List<Recommendation> recommendations = integration.getRecommendations(productId);
+
+        List<Review> reviews = integration.getReviews(productId);
+
+        return createProductAggregate(product, recommendations, reviews, serviceUtil.getServiceAddress());
+    }
+
+    private ProductAggregate createProductAggregate(Product product, List<Recommendation> recommendations, List<Review> reviews, String serviceAddress) {
+
+        // 1. Setup product info
+        int productId = product.getProductId();
+        String name = product.getName();
+        int weight = product.getWeight();
+
+        // 2. Copy summary recommendation info, if available
+        List<RecommendationSummary> recommendationSummaries = (recommendations == null) ? null :
+             recommendations.stream()
+                .map(r -> new RecommendationSummary(r.getRecommendationId(), r.getAuthor(), r.getRate()))
+                .collect(Collectors.toList());
+
+        // 3. Copy summary review info, if available
+        List<ReviewSummary> reviewSummaries = (reviews == null)  ? null :
+            reviews.stream()
+                .map(r -> new ReviewSummary(r.getReviewId(), r.getAuthor(), r.getSubject()))
+                .collect(Collectors.toList());
+
+        // 4. Create info regarding the involved microservices addresses
+        String productAddress = product.getServiceAddress();
+        String reviewAddress = (reviews != null && reviews.size() > 0) ? reviews.get(0).getServiceAddress() : "";
+        String recommendationAddress = (recommendations != null && recommendations.size() > 0) ? recommendations.get(0).getServiceAddress() : "";
+        ServiceAddresses serviceAddresses = new ServiceAddresses(serviceAddress, productAddress, reviewAddress, recommendationAddress);
+
+        return new ProductAggregate(productId, name, weight, recommendationSummaries, reviewSummaries, serviceAddresses);
+    }
+}
---
+++ util/src/main/java/by/ilya/util/exceptions/NotFoundException.java
@@ -0,0 +1,18 @@
+package by.ilya.util.exceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException() {
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
